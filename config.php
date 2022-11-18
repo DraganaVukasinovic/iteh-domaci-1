a4599b8482ef4bfa9ca1ddd69e76eba8f19adeec
DraganaVukasinovic <vukasinovic.dragana@yahoo.com>
2022-11-18 14:26:00 +0100

    kreirana konfiguracija za bazu

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+ 
+     
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "nakit";
+        
+        $conn = mysqli_connect($hostname,$username,$password,$database) or die("Database connection failed");
+     
+
+?>
