855239ccffce19cf6823b17eab9d3629a8c4d889
DraganaVukasinovic <vukasinovic.dragana@yahoo.com>
2022-11-18 16:33:54 +0100

    izmene na pocetnoj stranici

--- pocetna.php
+++ pocetna.php
@@ -1,9 +1,5 @@
 <?php
-    include 'config.php'; //moramo zbog $conn
-    include 'model/Nakit.php'; //moramo zbog klase Nakit
-     
-    //da bismo mogli da prikazemo sav nakit u tabeli moramo da prvo procitamo sve podatke o svom nakitu iz baze
-    $savNakit = Nakit::vratiSavnakit($conn); //rezultat ovog upita cemo prikazati u tabeli dole
+ 
   
     
 
@@ -20,53 +16,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <style>
-        .card {
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-            max-width: 300px;
-            margin: auto;
-            text-align: center;
-            font-family: arial;
-            padding: 10px;
-        }
-
-        .price {
-            color: grey;
-            font-size: 22px;
-        }
-
-        .card button {
-            border: none;
-            outline: 0;
-            padding: 12px;
-            color: white;
-            background-color: #000;
-            text-align: center;
-            cursor: pointer;
-            width: 100%;
-            font-size: 18px;
-        }
-
-        .card button:hover {
-             opacity: 0.7;
-        }
-        .sav-nakit{
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            
-            
-            margin-left: 9%;
-            margin-right:9%;
-            margin-top: 10%;
-            justify-content: space-between;
-        }
-        .kategorija {
-            height: 25px;
-            width: 100px;
-            background-color: #9E9DAD;
-            border-radius: 10px;
-            display: inline-block;
-        }
+        
     </style>
 
 
@@ -84,20 +34,13 @@
             </form>
         </nav>
 
-        <div class="sav-nakit">
 
-            <?php   while($red = $savNakit->fetch_array()):  ?>
-                <div class="card">
-                    <img src=<?php echo $red["slika"]?>  style="width:100%">
-                    <h5> <?php echo $red["naziv"]?>  </h5>
-                    <span class="kategorija"> <?php echo $red["nazivKategorije"]?></span>
-                    <p class="price"> <?php echo $red["cena"]?></p>
-                    <p>  <?php echo $red["opis"]?></p>
-                     
-                </div>
 
-            <?php endwhile;?>
-        </div>
+        
+    <?php
+        include 'savNakit.php';
+    
+    ?>
 
 
 
